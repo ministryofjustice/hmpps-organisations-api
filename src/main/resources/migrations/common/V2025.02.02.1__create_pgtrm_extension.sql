8c59f44cbe6bd2724574952515c4a59b2b03077a
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2025-02-05 17:19:09 +0000

    PRC-436: Fixing ordering of creating the pg_trm extension (#5)

---
+++ src/main/resources/migrations/common/V2025.02.02.1__create_pgtrm_extension.sql
@@ -0,0 +1,4 @@
+--
+-- enable pg_trgm (for GIN indexes)
+--
+CREATE EXTENSION pg_trgm;
--- src/main/resources/migrations/common/V2025.02.03.1__organisation_tables.sql
+++ src/main/resources/migrations/common/V2025.02.03.1__organisation_tables.sql
@@ -27,11 +27,6 @@ CREATE INDEX idx_organisation_organisation_name ON organisation(organisation_nam
 CREATE INDEX idx_organisation_created_time ON organisation(created_time);
 
 --
--- enable pg_trgm (for GIN indexes)
---
-CREATE EXTENSION pg_trgm;
-
---
 -- Add GIN index for organisation search
 --
 CREATE INDEX idx_organisation_name_gin ON organisation USING gin (organisation_name gin_trgm_ops);
