d6233b46a7848f40f9fc3cccf8c8224f2d0631db
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2025-02-28 08:50:50 +0000

    PRC-500: Correct typo

--- src/main/kotlin/uk/gov/justice/digital/hmpps/organisationsapi/model/response/sync/SyncOrganisationId.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/organisationsapi/model/response/sync/SyncOrganisationId.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.organisationsapi.model.response.sync
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "Response object for email address changes via sync")
+@Schema(description = "Response object for sync reconciliation")
 data class SyncOrganisationId(
   @Schema(description = "The ID for an organisation", example = "111111")
   val organisationId: Long,
