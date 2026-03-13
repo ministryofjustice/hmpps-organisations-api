5eb0e913ee7e61d06ba8dfbb4023d79041fca27a
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-03-13 12:41:51 +0000

    spring boot 4

--- src/main/kotlin/uk/gov/justice/digital/hmpps/organisationsapi/config/FeatureSwitches.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/organisationsapi/config/FeatureSwitches.kt
@@ -17,12 +17,13 @@ class FeatureSwitches(private val environment: Environment) {
 
   fun isEnabled(outboundEvent: OutboundEvent, defaultValue: Boolean = false): Boolean = get("feature.event.${outboundEvent.eventType}", Boolean::class.java, defaultValue)
 
-  private inline fun <reified T> get(property: String, type: Class<T>, defaultValue: T) = environment.getProperty(property, type).let {
-    if (it == null) {
+  private inline fun <reified T : Any> get(property: String, type: Class<T>, defaultValue: T): T {
+    val value = environment.getProperty(property, type)
+    return if (value == null) {
       log.info("property '$property' not configured, defaulting to $defaultValue")
       defaultValue
     } else {
-      it
+      value
     }
   }
 }
